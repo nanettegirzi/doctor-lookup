16066d26da12723889eba1284c6fb3499b7c999c
Nanette Girzi <ngirzi@gmail.com>
2018-03-23 15:58:53 -0700

    add search by issue and styling

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import './../.env';
+const apiKey = process.env.API_KEY;
+
 
 $(document).ready(function(){
   $("#doctorSearch").submit(function(event){
     event.preventDefault();
     let doctor = $("#doctor").val();
+    let issue = $("#issue").val();
+    $("#doctorSearch").hide();
+    $("#doctorResults").show();
 
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctor}&location=wa-seattle&skip=0&limit=100&user_key=714dd3c48597460d09b53ec4923e2736`).then(function(response){
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctor}&location=wa-seattle&skip=0&limit=100&user_key=${apiKey}`).then(function(response){
       let results = response.data;
       if(results.length == 0) {
         $("#doctorResults").append(`<h2>No doctors match your search. Please try again</h2>`);
@@ -28,7 +34,33 @@ $(document).ready(function(){
       });
     })
     .fail(function(error){
-      $("#doctorResults").append(`There was an error processing your request: ${error.responseText}. Please try again.`);
+      $("#doctorResults").text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
-  })
-})
+
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${issue}&location=wa-seattle&skip=0&limit=100&user_key=${apiKey}`).then(function(response){
+      let results = response.data;
+      if(results.length == 0) {
+        $("#doctorResults").append(`<h2>No doctors match your search. Please try again</h2>`);
+      }
+      results.forEach(function(result){
+        $("#issueResults").append(
+          `<h3>Name: ${result.profile.first_name} ${result.profile.last_name}</h3>
+          <p><strong>Address:</strong> ${result.practices[0].visit_address.street}, ${result.practices[0].visit_address.city}, ${result.practices[0].visit_address.state}, ${result.practices[0].visit_address.zip}</p>
+          <p><strong>Phone Number:</strong> ${result.practices[0].phones[0].number}</p?`
+        );
+        if(result.practices[0].accepts_new_patients) {
+          $("#issueResults").append(`<p><strong>Accepting New Patients:</strong> Yes</p> <hr>`);
+        } else{
+          $("#issueResults").append(`<p><strong>Accepting New Patients:</strong> No</p> <hr>`);
+        }
+      });
+    })
+    .fail(function(error){
+      $("#issueResults").text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+    });
+
+    $('button#home').click(function(){
+      location.reload();
+    })
+  });
+});
